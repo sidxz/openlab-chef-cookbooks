b552a4c7b1ba2e948a8c3f4cc7961d80d1e2b930
sidxz <sid@tamu.edu>
2016-10-07 13:51:50 -0500

    openlab-chef-client: Initial Commit

---
+++ openlab-chef-client/README.md
@@ -0,0 +1,4 @@
+# openlab-chef-client
+
+TODO: Enter the cookbook description here.
+
---
+++ openlab-chef-client/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: openlab-chef-client
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
