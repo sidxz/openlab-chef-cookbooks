acbafa8efde55cf8f5c6999b9b42c6116d9c58af
sidxz <sid@tamu.edu>
2016-10-07 13:56:41 -0500

    openlab-chef-client: chef-client bare

--- openlab-chef-client/recipes/default.rb
+++ openlab-chef-client/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+##########################
+# Run the chef-client rec
+##########################
+
+include_recipe 'chef-client::default'
