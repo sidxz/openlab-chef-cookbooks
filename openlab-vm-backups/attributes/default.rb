505019796256823f68a69055bc58d1dc9a0f705e
sidxz <sid@tamu.edu>
2016-10-11 23:40:24 -0500

    openlab-vms-backup:0.1.1 mount point fix

--- openlab-vm-backups/attributes/default.rb
+++ openlab-vm-backups/attributes/default.rb
@@ -1,5 +1,5 @@
 #Packages to install
-default['openlab-vm-backups']['packages'] = %w{rsync rsnapshot nfs-utils nfs-utils-lib}
+default['openlab-vm-backups']['packages'] = %w{epel-release rsync rsnapshot nfs-utils }
 
 #Mount points
 default['openlab-vm-backups']['mount-point']['backup'] = "/mnt/vms"
--- openlab-vm-backups/metadata.rb
+++ openlab-vm-backups/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'sid@tamu.edu'
 license 'all_rights'
 description 'Installs/Configures openlab-vm-backups'
 long_description 'Installs/Configures openlab-vm-backups'
-version '0.1.0'
+version '0.1.1'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
