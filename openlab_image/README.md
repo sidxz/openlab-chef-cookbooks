6e2587552f207d5e99c31ce6fae8f736f4901a78
sidxz <sid@tamu.edu>
2016-11-10 23:08:11 -0600

    openlab_image: Initial commit

---
+++ openlab_image/README.md
@@ -0,0 +1,4 @@
+# openlab_image
+
+TODO: Enter the cookbook description here.
+
---
+++ openlab_image/metadata.rb
@@ -0,0 +1,15 @@
+name 'openlab_image'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures openlab_image'
+long_description 'Installs/Configures openlab_image'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/openlab_image/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/openlab_image' if respond_to?(:source_url)
