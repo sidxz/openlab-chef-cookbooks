816749172bd8e4bb94a65bbd966e533b8e9c5c46
sidxz <sid@tamu.edu>
2016-11-11 18:39:46 -0600

    openlab_image: complete

--- openlab_image/recipes/install.rb
+++ openlab_image/recipes/install.rb
@@ -27,40 +27,82 @@ projectParams = node['openlab-global']['project-params']
 #Create the glance user:
 execute "user-create-glance" do
   command <<-EOH
-  openstack user create --domain default --password=#{node['openlab-image']['install']['glance-user-pass']} #{credentials} #{projectParams} glance
+  openstack user create --domain default --password=#{node['openlab-image']['install']['glance-user-pass']} #{credentials} #{projectParams} glance && touch /root/.chefvars/openlab_image-user-create-glance.bool
   EOH
   live_stream true
+  not_if {::File.exist?("/root/.chefvars/openlab_image-user-create-glance.bool")}
 end
 
 #Add the admin role to the glance user and service project
 execute "add-glance-to-service-project" do
   command <<-EOH
-  openstack role add --project service --user glance #{credentials} #{projectParams} admin
+  openstack role add --project service --user glance #{credentials} #{projectParams} admin && touch /root/.chefvars/openlab_image-add-glance-to-service-project.bool
   EOH
   live_stream true
+  not_if {::File.exist?("/root/.chefvars/openlab_image-add-glance-to-service-project.bool")}
 end
 
 #Create the glance service entity
 execute "service-create-glance" do
   command <<-EOH
-  openstack service create --name glance --description "Openstack image" #{credentials} #{projectParams} image
+  openstack service create --name glance --description "Openstack image" #{credentials} #{projectParams} image && touch /root/.chefvars/openlab_image-service-create-glance.bool
   EOH
   live_stream true
+  not_if {::File.exist?("/root/.chefvars/openlab_image-service-create-glance.bool")}
 end
 
 %w(public internal admin).each do |key|
-	execute "service-create-glance" do
+	execute "endpoint-create-glance" do
   		command <<-EOH
-  openstack endpoint create --region RegionOne image #{key} http://openlab-controller:9292 #{credentials} #{projectParams}
+  openstack endpoint create --region RegionOne image #{key} http://openlab-controller:9292 #{credentials} #{projectParams} && touch /root/.chefvars/openlab_image-endpoint-create-glance.bool
   EOH
   	live_stream true
-	end	
+    not_if {::File.exist?("/root/.chefvars/openlab_image-endpoint-create-glance.bool")}
+	end 	
 end
 
 #Install glance
-package "openstack-glance" do
+package "glance" do
   action [ :install ]
 end
 
+#Configure glance
+template "/etc/glance/glance-api.conf" do
+  source "glance-api.conf.erb"
+  owner 'glance'
+  group 'glance'
+  mode 0711
+  variables :glance_dbpass => node['openlab-image']['install']['glance-dp-pass'], :glance_user_pass => node['openlab-image']['install']['glance-user-pass']
+  notifies :restart, 'service[glance-registry]', :delayed
+  notifies :restart, 'service[glance-api]', :delayed
+end
 
+template "/etc/glance/glance-registry.conf" do
+  source "glance-registry.conf.erb"
+  owner 'glance'
+  group 'glance'
+  mode 0711
+  variables :glance_dbpass => node['openlab-image']['install']['glance-dp-pass'], :glance_user_pass => node['openlab-image']['install']['glance-user-pass']
+  notifies :restart, 'service[glance-registry]', :delayed
+  notifies :restart, 'service[glance-api]', :delayed
+end
+
+execute "glance_db_sync" do
+  command "su -s /bin/sh -c \"glance-manage db_sync\" glance && touch /root/.chefvars/openlab_identity-install-glance-dbsync.bool"
+  live_stream true
+  not_if {::File.exist?("/root/.chefvars/openlab_identity-install-glance-dbsync.bool")}
+  notifies :restart, 'service[glance-registry]', :delayed
+  notifies :restart, 'service[glance-api]', :delayed
+end
+
+
+#SERVICES
+
+service "glance-registry" do
+  action :nothing
+end
+
+service "glance-api" do
+  action :nothing
+end
 
