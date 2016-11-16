9153659c43c72582b37a4a7bc07b9321b0ad541f
sidxz <sid@tamu.edu>
2016-11-15 23:47:20 -0600

    com_rabbitmq : using global vars

--- com_rabbitmq/attributes/default.rb
+++ com_rabbitmq/attributes/default.rb
@@ -1,1 +1,2 @@
-default['com_rabbitmq']['rabbit_pass'] = 'WatchDogs@2'
+# Declared in global
+#default['com_rabbitmq']['rabbit_pass'] = 'WatchDogs@2'
--- com_rabbitmq/metadata.rb
+++ com_rabbitmq/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures com_rabbitmq'
 long_description 'Installs/Configures com_rabbitmq'
-version '0.1.0'
+version '0.1.1'
 
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Issues` link
@@ -13,3 +13,5 @@ version '0.1.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/com_rabbitmq' if respond_to?(:source_url)
+
+depends 'openlab_global'
