b6a4dceedeb6c8519c967e080f1efb6f81419606
sidxz <sid@tamu.edu>
2016-11-15 23:57:34 -0600

    openlab_global: changed rabit_mq pass

--- openlab_global/attributes/default.rb
+++ openlab_global/attributes/default.rb
@@ -12,4 +12,4 @@ default['openlab-identity']['openstack-cli']['OS_IDENTITY_API_VERSION'] = '3'
 default['openlab-global']['admin-credentials'] = "--os-username=#{default['openlab-identity']['openstack-cli']['OS_USERNAME']} --os-password=#{default['openlab-identity']['openstack-cli']['OS_PASSWORD']} "
 default['openlab-global']['project-params'] = "--os-project-name=#{default['openlab-identity']['openstack-cli']['OS_PROJECT_NAME']} --os-user-domain-name=#{default['openlab-identity']['openstack-cli']['OS_USER_DOMAIN_NAME']} --os-project-domain-name=#{default['openlab-identity']['openstack-cli']['OS_PROJECT_DOMAIN_NAME']} --os-auth-url=\"#{default['openlab-identity']['openstack-cli']['OS_AUTH_URL']}\" --os-identity-api-version=#{default['openlab-identity']['openstack-cli']['OS_IDENTITY_API_VERSION']}"
 
-default['com_rabbitmq']['rabbit_pass'] = 'WatchDogs@2'
+default['com_rabbitmq']['rabbit_pass'] = 'WatchDogs2'
