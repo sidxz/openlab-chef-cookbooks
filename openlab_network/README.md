048b3851f10a672c418fbb38415fc94dfda1d268
sidxz <sid@tamu.edu>
2016-11-22 23:06:58 -0600

    openlab-network: Initial Commit

---
+++ openlab_network/README.md
@@ -0,0 +1,4 @@
+# openlab_network
+
+TODO: Enter the cookbook description here.
+
---
+++ openlab_network/metadata.rb
@@ -0,0 +1,15 @@
+name 'openlab_network'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures openlab_network'
+long_description 'Installs/Configures openlab_network'
+version '0.1.0'
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Issues` link
+# issues_url 'https://github.com/<insert_org_here>/openlab_network/issues' if respond_to?(:issues_url)
+
+# If you upload to Supermarket you should set this so your cookbook
+# gets a `View Source` link
+# source_url 'https://github.com/<insert_org_here>/openlab_network' if respond_to?(:source_url)
